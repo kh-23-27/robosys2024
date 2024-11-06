b5ac57709fcc2a3108a879e17f8a706309af4624
Kenta Hirachi <s23c1114qb@s.chibakoudai.jp>
2024-11-07 06:15:30 +0900

    docs:名前変更

--- test.bash
+++ test.bash
@@ -1,7 +1,28 @@
-#!/bin/bash
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2024 Kenta Hirachi
+# SPDX-License=Identifier: BSD-3-Clause
+ng () {
+	echo ${1}行目が違うよ
+	res=1
+}
 
-a=yamada
-[ "$a" = ueda ]
-echo $?
-[ "$a" = yamada ]
-echo $?
+res=0
+
+### NORMAL INPUT ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ] 	  || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "${res}" = 0 ] &&  echo OK
+exit $res
+
+
