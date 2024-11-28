31dd264de9733c94a524f039e0b74c94cd6cc999
Kenta Hirachi <s23c1114qb@s.chibakoudai.jp>
2024-11-29 01:24:32 +0900

    fix: test2の修正

--- test2.bash
+++ test2.bash
@@ -8,23 +8,23 @@ ng () {
 
 res=0
 
-#サイコロを５回振るったとき
+#正常な動作
 out=$(echo 5 | ./dice_roll)
-[ "${out}" -ge 5 ] && [ "$out" -le 60 ] || ng "$LINENO"
+out=$(echo "$out" | grep '合計値:' | awk '{print $2}' | tr -d '\n')
+
+[ "${out}" -ge 5 ] && [ "$out" -le 30 ] || ng "$LINENO"
 
 #負の数を入力したとき
 out=$(echo -2 | ./dice_roll)
 [ "${out}" = "0以上の数字を入力してください。" ] || ng "$LINENO"
+
 #文字を入力したとき
 out=$(echo い | ./dice_roll)
-
-[ "$?" = 1 ]      || ng "$LINENO"
-[ "${out}" = "エラー:有効な数字を入力してください。" ] || ng "$LINENO"
+[ "${out}" = "エラー: 有効な数字を入力してください。" ] || ng "$LINENO"
 
 #空白を入力したとき
 out=$(echo | ./dice_roll)
-[ "$?" = 1 ] 	  || ng "$LINENO"
-[ "${out}" = "エラー:有効な数字を入力してください。" ] || ng "$LINENO"
+[ "${out}" = "エラー: 有効な数字を入力してください。" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] &&  echo OK
 exit $res
